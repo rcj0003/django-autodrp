6db9f5a7f1793671ac93fa7b6a8b9326a149862a
Ryan Jones <rcj0399@gmail.com>
2021-11-05 11:35:26 -0400

    Incremental

--- autodrp/utils.py
+++ autodrp/utils.py
@@ -1,4 +1,6 @@
 from django.db.models import Q
+from functools import wraps
+import inspect
 
 from .checks import QueryBuilderCheck
 
@@ -17,61 +19,65 @@ class EvalArgument:
         self._validate_kwargs(kwargs)
         return eval(self._compiled_eval, globals={}, locals=kwargs)
 
+def _compile_and(q_obj=None, compiled_q=None, **kwargs):
+    if not compiled_q:
+        compiled_q = Q(**kwargs)
+
+    if q_obj:
+        return q_obj & compiled_q
+    return compiled_q
+
+def _compile_and_not(q_obj=None, compiled_q=None, **kwargs):
+    if not compiled_q:
+        compiled_q = Q(**kwargs)
+
+    if q_obj:
+        return q_obj & ~compiled_q
+    return compiled_q
+
+def _compile_or(q_obj=None, compiled_q=None, **kwargs):
+    if not compiled_q:
+        compiled_q = Q(**kwargs)
+
+    if q_obj:
+        return q_obj | compiled_q
+    return compiled_q
+
+def _compile_or_not(q_obj=None, compiled_q=None, **kwargs):
+    if not compiled_q:
+        compiled_q = ~Q(**kwargs)
+
+    if q_obj:
+        return q_obj | compiled_q
+    return compiled_q
+
+def _compile_xor(q_obj=None, compiled_q=None, **kwargs):
+    if not compiled_q:
+        arg1, arg2 = kwargs.items()
+        arg1, arg2 = {arg1[0]: arg1[1]}, {arg2[0]: arg2[1]}
+        compiled_q = (Q(**arg1) and ~Q(**arg2)) | (Q(**arg2) and ~Q(**arg1))
+
+    if q_obj:
+        return q_obj & compiled_q
+
+    return compiled_q
+
+def _compile_args(operation_kwargs, **kwargs):
+    compiled_kwargs = {key: value if not callable(value) else value(**kwargs) for key, value in operation_kwargs.items()}
+    return compiled_kwargs
+
 class QueryBuilder:
     @staticmethod
     def where(**kwargs):
         return QueryBuilder().and_where(**kwargs)
+    
+    @staticmethod
+    def where_not(**kwargs):
+        return QueryBuilder().and_not_where(**kwargs)
 
     def __init__(self):
         self._query = []
         self._cached_q = None
-
-    def _compile_and(q_obj=None, compiled_q=None, **kwargs):
-        if not compiled_q:
-            compiled_q = Q(**kwargs)
-
-        if q_obj:
-            return q_obj & compiled_q
-        return compiled_q
-
-    def _compile_and_not(q_obj=None, compiled_q=None, **kwargs):
-        if not compiled_q:
-            compiled_q = Q(**kwargs)
-
-        if q_obj:
-            return q_obj & ~compiled_q
-        return compiled_q
-
-    def _compile_or(q_obj=None, compiled_q=None, **kwargs):
-        if not compiled_q:
-            compiled_q = Q(**kwargs)
-
-        if q_obj:
-            return q_obj | compiled_q
-        return compiled_q
-
-    def _compile_or_not(q_obj=None, compiled_q=None, **kwargs):
-        if not compiled_q:
-            compiled_q = ~Q(**kwargs)
-
-        if q_obj:
-            return q_obj | compiled_q
-        return compiled_q
-    
-    def _compile_xor(q_obj=None, compiled_q=None, **kwargs):
-        if not compiled_q:
-            arg1, arg2 = kwargs.items()
-            arg1, arg2 = {arg1[0]: arg1[1]}, {arg2[0]: arg2[1]}
-            compiled_q = (Q(**arg1) and ~Q(**arg2)) | (Q(**arg2) and ~Q(**arg1))
-
-        if q_obj:
-            return q_obj & compiled_q
-
-        return compiled_q
-    
-    def _compile_args(self, operation_kwargs, **kwargs):
-        compiled_kwargs = {key: value if not callable(value) else value(**kwargs) for key, value in operation_kwargs.items()}
-        return compiled_kwargs
 
     def _add_operation(self, operation, **kwargs):
         if not any(callable(value) for value in kwargs.values()):
@@ -89,25 +95,25 @@ class QueryBuilder:
         })
      
     def and_where(self, **kwargs):
-        self._add_operation(self._compile_and, **kwargs)
+        self._add_operation(_compile_and, **kwargs)
         return self
 
     def and_not_where(self, **kwargs):
-        self._add_operation(self._compile_and_not, **kwargs)
+        self._add_operation(_compile_and_not, **kwargs)
         return self
         
     def or_where(self, **kwargs):
-        self._add_operation(self._compile_or, **kwargs)
+        self._add_operation(_compile_or, **kwargs)
         return self
     
     def or_not_where(self, **kwargs):
-        self._add_operation(self._compile_or_not, **kwargs)
+        self._add_operation(_compile_or_not, **kwargs)
         return self
     
     def where_one_not_both(self, **kwargs):
         if len(kwargs) != 2:
             raise Exception('XOR requires ONLY a left and right operand (2 keyword arguments required)')
-        self._add_operation(self._compile_xor, **kwargs)
+        self._add_operation(_compile_xor, **kwargs)
         return self
     
     def build(self, do_cache=False, ignore_cache=False, **kwargs):
@@ -123,7 +129,7 @@ class QueryBuilder:
 
             if not compiled_q:
                 should_cache = False
-                op_kwargs = self._compile_args(**kwargs)
+                op_kwargs = _compile_args(**kwargs)
                 if len(op_kwargs) == 0 and operation['ignore_none_args']:
                     continue
                 q_obj = operation_func(q_obj=q_obj, **op_kwargs)
@@ -143,20 +149,52 @@ class QueryBuilder:
         return QueryBuilderCheck(self)
 
 class filter_keyword_args:
-    def __init__(self, function, *keywords):
-        self.function = function
-        self.keywords = keywords
-        self.keyword_count = len(keywords)
+    def __init__(self):
+        self._function_signature = None
     
-    def __call__(self, *args, **kwargs):
-        if len(kwargs) == 0 and len(args) == self.keyword_count:
-            return self.function(*args)
+    def _parse_args(self, *args, **kwargs):
+        if self.accepts_var_args:
+            required_args = args[0:self.required_args]
+            var_args = args[self.required_args:]
+        else:
+            args_passed = len(args)
+            if args_passed != self.required_args:
+                raise Exception(f'Method requires exactly {self.required_args} arguments ({args_passed} arguments were used)')
+            required_args = args
+            var_args = []
 
-        values = []
-        for key in self.keywords:
-            value = kwargs.get(key)
-            if not value:
-                raise Exception(f'Missing keyword argument {key}.')
-            values.append(value)
+        missing_kwargs = []
+        for keyword in self.required_keywords:
+            if keyword not in kwargs:
+                missing_kwargs.append(keyword)
         
-        return self.function(*values)
+        if missing_kwargs:
+            raise Exception(f'Method requires the keywords {", ".join(self.required_keywords)} arguments ({", ".join(missing_kwargs)} are missing)')
+
+        if self.accepts_var_kwargs:
+            accepted_kwargs = kwargs
+        else:
+            accepted_kwargs = {key: kwargs[key] for key in self.required_keywords}
+
+        return required_args, var_args, accepted_kwargs
+
+    def __call__(self, func):
+        self._function_signature = inspect.signature(func)
+
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            if any(param.kind == param.VAR_KEYWORD for param in self._function_signature.parameters.values()):
+                bound_args = self._function_signature.bind(*args, **kwargs)
+                bound_args.apply_defaults()
+                return func(**bound_args.arguments)
+            else:
+                kwargs = {
+                    param_name: kwargs[param_name]
+                    for param_name, param in self._function_signature.parameters.items() if (
+                        param.kind is param.KEYWORD_ONLY or
+                        param.kind is param.POSITIONAL_OR_KEYWORD
+                    ) and
+                    param_name in kwargs
+                }
+                return func(*args, **kwargs)
+        return wrapped_function
